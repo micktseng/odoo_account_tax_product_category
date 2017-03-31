f2439f93129b1c040b17c912a908e8cd6225639b
Mick <suncombo@gmail.com>
2017-03-31 10:11:42 +0800

    ADD Files

---
+++ models/product.py
@@ -0,0 +1,70 @@
+from openerp import api, fields, models
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+    _parent_store = False
+
+    @api.one
+    def _get_total_taxes(self):
+        categ = self
+        customer_taxes = categ.taxes_id
+        supplier_taxes = categ.supplier_taxes_id
+        while categ.parent_id and (not customer_taxes or not supplier_taxes):
+            categ = categ.parent_id
+            if not customer_taxes:
+                customer_taxes |= categ.taxes_id
+            if not supplier_taxes:
+                supplier_taxes |= categ.supplier_taxes_id
+        self.total_taxes_id = customer_taxes
+        self.total_supplier_taxes_id = supplier_taxes
+
+    taxes_id = fields.Many2many('account.tax', 'product_category_taxes_rel', 'categ_id', 'tax_id', 'Customer Taxes',
+                                domain=[('type_tax_use', 'in', ['sale', 'all'])],
+                                help="Taxes change in product category is not propagated in existing products")
+    supplier_taxes_id = fields.Many2many('account.tax', 'product_category_supplier_taxes_rel', 'categ_id', 'tax_id', 'Supplier Taxes',
+                                         domain=[('type_tax_use', 'in', ['purchase', 'all'])],
+                                         help="Taxes change in product category is not propagated in existing products")
+    total_taxes_id = fields.Many2many('account.tax', string='Inherited Customer Taxes', compute='_get_total_taxes')
+    total_supplier_taxes_id = fields.Many2many('account.tax', string='Inherited Supplier Taxes', compute='_get_total_taxes')
+
+    @api.multi
+    def write(self, vals):
+        old_taxes_to_keep = {}
+        company = self.env.user.company_id
+        for field in ('taxes_id', 'supplier_taxes_id'):
+            if field in vals:
+                for categ in self.sudo():
+                    old_taxes_to_keep.setdefault(field, {})
+                    old_taxes_to_keep[field][categ] = getattr(categ, field).filtered(lambda tax: tax.company_id != company)
+        res = super(ProductCategory, self).write(vals)
+        for field in old_taxes_to_keep:
+            for categ in old_taxes_to_keep[field]:
+                taxes = getattr(categ, field)
+                taxes |= old_taxes_to_keep[field][categ]
+        return res
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.onchange('categ_id')
+    def _onchange_categ_id(self):
+        if self.categ_id:
+            self.taxes_id = self.categ_id.total_taxes_id
+            self.supplier_taxes_id = self.categ_id.total_supplier_taxes_id
+
+    @api.model
+    def create(self, vals):
+        product_tmpl = super(ProductTemplate, self).create(vals)
+        if vals.get('categ_id') and 'taxes_id' not in vals and 'supplier_taxes_id' not in vals:
+            product_tmpl._onchange_categ_id()
+        return product_tmpl
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.onchange('categ_id')
+    def _onchange_categ_id(self):
+        self.product_tmpl_id._onchange_categ_id()
